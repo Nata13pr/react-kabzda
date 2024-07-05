0d1761189a3e0cfc5293ca0ca531bcd29563ef89
nata13pr <Nata13pr@gmail.com>
2024-07-05 15:48:32 +0300

    feat:add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -16,7 +16,7 @@ class Users extends Component {
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
             })
