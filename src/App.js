1de85f4cb33ce6149e8f9eb332562176c76a56dc
nata13pr <Nata13pr@gmail.com>
2024-07-16 18:58:10 +0300

    feat: add React.lazy, React.Suspense

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import {Component} from "react";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
-import {NavbarContainer} from "./components/Navbar/NavbarContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import Preloader from "./common/preloader/Preloader";
@@ -17,7 +14,12 @@ import {initializeApp} from "./redux/app-reducer";
 import withRouter from "./components/Profile/withRouter";
 import './App.css';
 import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withSuspense";
+import {NavbarContainer} from "./components/Navbar/NavbarContainer";
 
+const DialogsContainer=React.lazy(() => import("./components/Dialogs/DialogsContainer"));
+const ProfileContainer=React.lazy(() => import("./components/Profile/ProfileContainer"));
+const UsersContainer=React.lazy(() => import("./components/Users/UsersContainer"));
 
 class App extends Component {
 
@@ -43,14 +45,13 @@ class App extends Component {
                             }/>
                         <Route
                             path="/dialogs/*"
-                            element={
-                                <DialogsContainer
-                                />
+                            element={withSuspense(DialogsContainer
+                            )
+
                             }/>
                         <Route
                             path="/profile/:userId?"
-                            element={
-                                <ProfileContainer/>
+                            element={withSuspense(ProfileContainer())
                             }/>
                         <Route
                             path="/news"
@@ -69,9 +70,7 @@ class App extends Component {
                             }/>
                         <Route
                             path="/users"
-                            element={
-                                <UsersContainer
-                                />
+                            element={withSuspense(UsersContainer)
                             }/>
                     </Routes>
                 </div>
