9a0672fb11934a4f87a09c6d857be4c5ca1c32e5
nata13pr <Nata13pr@gmail.com>
2024-07-16 18:49:17 +0300

    feat: unit tests Paginator,add proper pagination

--- src/redux/user-reducer.js
+++ src/redux/user-reducer.js
@@ -11,7 +11,7 @@ const TOGGLE_IS_FOLLOWING = 'TOGGLE-IS-FOLLOWING';
 
 const initialState = {
     users: [],
-    pageSize: 50,
+    pageSize: 10,
     totalUsersCount: 0,
     currentPage: 1,
     isFetching: false,
