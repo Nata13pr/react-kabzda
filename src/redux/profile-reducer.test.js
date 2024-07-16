54d7ba090ce09ac0e6d17461b1664dcc09419b11
nata13pr <Nata13pr@gmail.com>
2024-07-16 15:33:56 +0300

    feat: unit tests jest

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,53 +1,55 @@
-// import profileReducer, {addPost, deletePost} from "./profileReducer";
-//
-// let state = {
-//     posts: [
-//         {id: 1, message: 'Hi, how are you?', likesCount: 12},
-//         {id: 2, message: 'It\'s my first post', likesCount: 11},
-//         {id: 3, message: 'Blabla', likesCount: 11},
-//         {id: 4, message: 'Dada', likesCount: 11}
-//     ]
-// };
-//
-// it('length of posts should be incremented', () => {
-// //    1.test data
-//     let action = addPost(
-//         'it-kamasutra.com')
-//
-//     //2.action
-//     let newState = profileReducer(state, action);
-//     //3.expectation
-//     expect(newState.posts.length).toBe(5)
-// })
-// it('message of new post should be correct', () => {
-//     // 1. test data
-//     let action = addPost("it-kamasutra.com");
-//
-//     // 2. action
-//     let newState = profileReducer(state, action);
-//
-//     // 3. expectation
-//     expect(newState.posts[4].message).toBe("it-kamasutra.com");
-// });
-//
-// it('after deleting length of messages should be decrement', () => {
-//     // 1. test data
-//     let action = deletePost(1);
-//
-//     // 2. action
-//     let newState = profileReducer(state, action);
-//
-//     // 3. expectation
-//     expect(newState.posts.length).toBe(3);
-// });
-//
-// it(`after deleting length shouldn't be decrement if id is incorrect`, () => {
-//     // 1. test data
-//     let action = deletePost(1000);
-//
-//     // 2. action
-//     let newState = profileReducer(state, action);
-//
-//     // 3. expectation
-//     expect(newState.posts.length).toBe(4);
-// });
+import profileReducer, {addPost, deletePost} from "./profile-reducer.js";
+
+
+let state = {
+    posts: [
+        {id: 1, post: 'I am new here', likeCount: '9'},
+        {id: 2, post: 'Have you been in Greece', likeCount: '55'},
+        {id: 3, post: 'I want to work', likeCount: '6'},
+        {id:4,post:'I need a vacation', likeCount: '7'},
+    ],
+};
+
+it('length of posts should be incremented', () => {
+//    1.test data
+    let action = addPost(
+        'it-kamasutra.com')
+
+    //2.action
+    let newState = profileReducer(state, action);
+    //3.expectation
+    expect(newState.posts.length).toBe(5)
+})
+it('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPost("it-kamasutra.com");
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts[4].post).toBe("it-kamasutra.com");
+});
+
+it('after deleting length of messages should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1);
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts.length).toBe(3);
+});
+
+it(`after deleting length shouldn't be decrement if id is incorrect`, () => {
+    // 1. test data
+    let action = deletePost(1000);
+
+    // 2. action
+    let newState = profileReducer(state, action);
+
+    // 3. expectation
+    expect(newState.posts.length).toBe(4);
+});
+
